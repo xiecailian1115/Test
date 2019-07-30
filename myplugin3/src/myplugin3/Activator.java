3539bc1dd99593afd66e8013314e464f935014e7
xiecailian1115 <cailianxie@outlook.com>
2019-07-30 19:45:47 +0800

    jira缺陷YFCS-145:nasdf 测试

--- myplugin3/src/myplugin3/Activator.java
+++ myplugin3/src/myplugin3/Activator.java
@@ -5,6 +5,8 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
+	
+	
 	@Override
 	public void start(BundleContext context) throws Exception {
 		System.out.println("Hello World!!");
